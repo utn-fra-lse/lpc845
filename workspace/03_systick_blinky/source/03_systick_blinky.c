a009a4f0807b361bde75d4610a3f89bbeb538562
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2024-06-06 20:57:31 -0300

    feat: blinky con systick

---
+++ workspace/03_systick_blinky/source/03_systick_blinky.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "LPC845.h"
+#include "fsl_debug_console.h"
+
+// Pin para el LED azul
+#define LED_BLUE	1
+
+/**
+ * @brief Programa principal
+ */
+int main(void) {
+
+	// Inicializacion
+	BOARD_BootClockFRO24M();
+
+    // Estructura de configuracion de salida
+    gpio_pin_config_t out_config = { kGPIO_DigitalOutput, 1 };
+
+    // Habilito el puerto 1
+    GPIO_PortInit(GPIO, 1);
+    // Configuro LED como salida
+    GPIO_PinInit(GPIO, 1, LED_BLUE, &out_config);
+
+    // Configuro SysTick para 1 ms
+    SysTick_Config(SystemCoreClock / 1000);
+
+    while(1);
+    return 0 ;
+}
+
+/**
+ * @brief Handler para interrupcion de SysTick
+ */
+void SysTick_Handler(void) {
+	// Variable para contar interrupciones
+	static uint16_t i = 0;
+
+	// Incremento contador
+	i++;
+	// Verifico si el SysTick se disparo 500 veces (medio segundo)
+	if(i == 500) {
+		// Reinicio el contador
+		i = 0;
+		// Conmuto el LED
+		GPIO_PinWrite(GPIO, 1, LED_BLUE, !GPIO_PinRead(GPIO, 1, LED_BLUE));
+	}
+}
+
