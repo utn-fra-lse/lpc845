4c90ea83cd029fda51c10bc1193b3a5c352491af
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2024-06-24 23:15:16 -0300

    feat: ejemplo

---
+++ workspace/01_hello_world/src/01_hello_world.c
@@ -0,0 +1,21 @@
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "fsl_debug_console.h"
+
+/*
+ * @brief   Application entry point.
+*/
+int main(void) {
+
+	// Inicializacion
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitBootPeripherals();
+    BOARD_InitDebugConsole();
+
+    PRINTF("Hello World\r\n");
+
+    while(1);
+    return 0;
+}
