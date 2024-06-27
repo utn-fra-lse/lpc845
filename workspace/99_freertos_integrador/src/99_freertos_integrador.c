645f65a0e85976eb1a4fbd6a1c21c0bb55eb703f
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2024-06-27 14:06:47 -0300

    feat: inicializo tareas y colas

--- workspace/99_freertos_integrador/src/99_freertos_integrador.c
+++ workspace/99_freertos_integrador/src/99_freertos_integrador.c
@@ -1,4 +1,8 @@
 #include "board.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "app_tasks.h"
 
 /**
  * @brief Programa principal
@@ -7,10 +11,35 @@ int main(void) {
 	// Clock del sistema a 30 MHz
 	BOARD_BootClockFRO30M();
 
+	// Configuro el ADC
+	wrapper_adc_init();
+	// Configuro el display
+	wrapper_display_init();
 
+	// Inicializo cola
+	queue_adc = xQueueCreate(1, sizeof(adc_data_t));
 
-    while(1) {
+	// Tarea de lectura de ADC
+	xTaskCreate(
+		task_adc_read,
+		"ADC Read",
+		tskADC_READ_STACK,
+		NULL,
+		tskADC_READ_PRIORITY,
+		NULL
+	);
 
-    }
-    return 0 ;
+	xTaskCreate(
+		task_display_write,
+		"Display Write",
+		tskDISPLAY_WRITE_STACK,
+		NULL,
+		tskDISPLAY_WRITE_PRIORITY,
+		NULL
+	);
+
+	vTaskStartScheduler();
+
+    while(1);
+    return 0;
 }
