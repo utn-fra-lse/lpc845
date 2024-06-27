37e324a8ab5ec392241a8331eb9a8621abf95b39
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2024-06-27 11:51:06 -0300

    feat: etiquetas para los segmentos

--- workspace/99_freertos_integrador/inc/labels.h
+++ workspace/99_freertos_integrador/inc/labels.h
@@ -6,4 +6,16 @@
 // Canal del ADC para el LM35
 #define LM35_CH		7
 
+// Etiquetas para los pines del 7 segmentos
+
+#define SEG_A	11
+#define SEG_B	10
+#define SEG_C	6
+#define SEG_D	14
+#define SEG_E	0
+#define SEG_F	13
+#define SEG_G	15
+#define COM_1	8
+#define COM_2	9
+
 #endif
