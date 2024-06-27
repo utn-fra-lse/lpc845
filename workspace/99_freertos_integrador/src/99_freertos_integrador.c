eb60e935759d2338d234c9ab968caeefc99d87e7
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2024-06-27 00:15:08 -0300

    chore: inicializacion del proyecto

---
+++ workspace/99_freertos_integrador/src/99_freertos_integrador.c
@@ -0,0 +1,16 @@
+#include "board.h"
+
+/**
+ * @brief Programa principal
+ */
+int main(void) {
+	// Clock del sistema a 30 MHz
+	BOARD_BootClockFRO30M();
+
+
+
+    while(1) {
+
+    }
+    return 0 ;
+}
