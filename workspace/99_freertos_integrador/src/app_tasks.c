07fc1ec21b70f9def3cf122a40b5957e1b081a88
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2024-06-27 11:50:35 -0300

    feat: inicializacion de display y cola de interrupcion

--- workspace/99_freertos_integrador/src/app_tasks.c
+++ workspace/99_freertos_integrador/src/app_tasks.c
@@ -1,10 +1,12 @@
 #include "app_tasks.h"
 
+// Cola para datos del ADC
+xQueueHandle queue_adc;
+
 /**
- * @brief Tarea de inicializacion
+ * @brief Inicializacion de perifericos
  */
 void task_init(void *params) {
-
 	// Configuro el ADC
 	wrapper_adc_init();
 
@@ -12,24 +14,43 @@ void task_init(void *params) {
 	vTaskDelete(NULL);
 }
 
+/**
+ * @brief Activa una secuencia de conversion cada 0.25 segundos
+ */
+void task_adc_read(void *params) {
+
+	while(1) {
+		// Inicio una conversion
+		ADC_DoSoftwareTriggerConvSeqA(ADC0);
+		// Bloqueo la tarea por 250 ms
+		vTaskDelay(250);
+	}
+}
 
 /**
  * @brief Handler para la interrupcion del ADC Sequence A
  */
 void ADC_SEQA_IRQHandler(void) {
+	// Variable de cambio de contexto
+	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
 	// Verifico que se haya terminado la conversion correctamente
 	if(kADC_ConvSeqAInterruptFlag == (kADC_ConvSeqAInterruptFlag & ADC_GetStatusFlags(ADC0))) {
 		// Limpio flag de interrupcion
 		ADC_ClearStatusFlags(ADC0, kADC_ConvSeqAInterruptFlag);
 		// Resultado de conversion
-		adc_result_info_t adc_info;
+		adc_result_info_t temp_info, ref_info;
 		// Leo el valor del ADC
-		ADC_GetChannelConversionResult(ADC0, REF_POT_CH, &adc_info);
-		// Leo potenciometro (0 a 30 grados)
-		float ref = 30 * adc_info.result / 4095.0;
+		ADC_GetChannelConversionResult(ADC0, REF_POT_CH, &ref_info);
 		// Leo el valor del ADC
-		ADC_GetChannelConversionResult(ADC0, LM35_CH, &adc_info);
-		// Leo el LM35
-		float temp = 330 * adc_info.result / 4095.0;
+		ADC_GetChannelConversionResult(ADC0, LM35_CH, &temp_info);
+		// Estructura de datos para mandar
+		adc_data_t data = {
+			.temp_raw = (uint16_t)temp_info.result,
+			.ref_raw = (uint16_t)ref_info.result
+		};
+		// Mando por la cola los datos
+		xQueueSendToBackFromISR(queue_adc, &data, &xHigherPriorityTaskWoken);
+		// Veo si hace falta un cambio de contexto
+		portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
 	}
 }
