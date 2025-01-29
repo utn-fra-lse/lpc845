c58a4a36037e0a6027eb2d71b9a3094d3a08a3be
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2025-01-29 13:46:29 -0300

    feat: migro a api nueva

--- ejemplos/01_blinky/README.md
+++
@@ -1,9 +0,0 @@
-# 01_blinky
-
-Programa ejemplo básico que hace parpadear el LED azul periodicamente.
-
-## Pines usados
-
-| Nombre | Pin |
-| --- | --- |
-| LED Blue | PIO1_1
--- ejemplos/01_blinky/src/01_blinky.c
+++ ejemplos/01_blinky/src/01_blinky.c
@@ -1,31 +1,25 @@
-#include "board.h"
-#include "peripherals.h"
-#include "pin_mux.h"
-#include "fsl_debug_console.h"
+#include "common/stdlib.h"
+#include "hardware/gpio.h"
 
 // Etiqueta para el LED azul
-#define LED_BLUE	1
+#define LED_BLUE	33
 
 /*
  * @brief   Application entry point.
 */
 int main(void) {
 	// Inicializacion
-    BOARD_InitBootClocks();
-    BOARD_InitDebugConsole();
+	stdio_init_all();
 
-    // Estructura de configuracion para salida
-    gpio_pin_config_t config = { kGPIO_DigitalOutput, 1 };
-    // Habilito el clock del GPIO 1
-    GPIO_PortInit(GPIO, 1);
-    // Configuro el pin 0 del GPIO 1 como salida
-    GPIO_PinInit(GPIO, 1, LED_BLUE, &config);
+	// Inicializacion de GPIO
+	gpio_init(LED_BLUE);
+	gpio_set_dir(LED_BLUE, true);
 
     while(1) {
     	// Cambio el estado anterior del LED azul
-    	GPIO_PinWrite(GPIO, 1, LED_BLUE, !GPIO_PinRead(GPIO, 1, LED_BLUE));
-    	// Demora
-    	for(uint32_t i = 0; i < 1000000; i++);
+    	gpio_put(LED_BLUE, !gpio_get(LED_BLUE));
+    	// Demora de 500 ms
+    	sleep_ms(500);
     }
     return 0;
 }
