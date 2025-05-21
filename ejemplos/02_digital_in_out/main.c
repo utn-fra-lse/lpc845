0c28d6a237fe04fb4c8ca81bafa7ec714ed62ff2
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2025-05-21 02:00:15 -0300

    feat: migro ejemplo a vscode

--- ejemplos/02_digital_in_out/main.c
+++ ejemplos/02_digital_in_out/main.c
@@ -1,19 +1,14 @@
 #include "board.h"
-#include "LPC845.h"
 
-// Etiqueta para el pulsador USER
-#define USER_BTN	4
-// Etiqueta para el LED azul
-#define LED_BLUE	1
+// Puntero a GPIO, numero de puerto y pin del LED azul
+#define LED_BLUE    GPIO, 1, 1
+// Puntero a GPIO, numero de puerto y pin del pulsador de USER
+#define USER_BTN    GPIO, 0, 4
 
-/*
- * @brief   Application entry point.
-*/
+/**
+ * @brief Programa principal
+ */
 int main(void) {
-
-	// Inicializacion
-    BOARD_InitBootClocks();
-
     // Estructura de configuracion para salida
     gpio_pin_config_t out_config = { kGPIO_DigitalOutput, 1 };
     // Estructura de configuracion para entrada
@@ -21,24 +16,22 @@ int main(void) {
     // Habilito el puerto 0
     GPIO_PortInit(GPIO, 0);
     // Configuro el pin 4 como entrada
-    GPIO_PinInit(GPIO, 0, USER_BTN, &in_config);
+    GPIO_PinInit(USER_BTN, &in_config);
     // Habilito el clock del GPIO 1
     GPIO_PortInit(GPIO, 1);
     // Configuro el Pin 0 del GPIO 1 como salida
-    GPIO_PinInit(GPIO, 1, LED_BLUE, &out_config);
+    GPIO_PinInit(LED_BLUE, &out_config);
 
     while(1) {
 
     	// Verifico el estado del pulsador (tiene pull-up)
-    	if(!GPIO_PinRead(GPIO, 0, USER_BTN)) {
+    	if(!GPIO_PinRead(USER_BTN)) {
     		// Prendo el LED (anodo comun)
-    		GPIO_PinWrite(GPIO, 1, LED_BLUE, 0);
+    		GPIO_PinWrite(LED_BLUE, 0);
     	}
     	else {
     		// Apago el LED
-    		GPIO_PinWrite(GPIO, 1, LED_BLUE, 1);
+    		GPIO_PinWrite(LED_BLUE, 1);
     	}
     }
-
-    return 0;
 }
