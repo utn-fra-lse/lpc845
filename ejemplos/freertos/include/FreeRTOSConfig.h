86e86a6ba5494be0c436338a1ab6041648513fa2
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2025-06-12 05:13:40 -0300

    chore: aumento de caracteres para nombres de tareas

--- ejemplos/freertos/include/FreeRTOSConfig.h
+++ ejemplos/freertos/include/FreeRTOSConfig.h
@@ -22,7 +22,7 @@
 #define configMAX_PRIORITIES							( 8 )
 #define configMINIMAL_STACK_SIZE					( ( uint16_t ) 64 )
 #define configTOTAL_HEAP_SIZE							( ( size_t ) ( 8192 ) )
-#define configMAX_TASK_NAME_LEN						( 10 )
+#define configMAX_TASK_NAME_LEN						( 16 )
 #define configUSE_TRACE_FACILITY					1
 #define configUSE_16_BIT_TICKS						0
 #define configIDLE_SHOULD_YIELD						1
